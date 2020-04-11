f973af06248ca31b419c935014ac61d738cbf3f3
Luke Barratt <luke.barratt@cpwonlinesolutions.com>
2020-04-11 11:46:06 +0100

    Layouts, functions and anonymous functions

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite colour?',
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text('This is my default text!'),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Answer 2 chosen!'),
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () => print('Answer 3 chosen!'),
+              )
+            ],
+          )),
     );
   }
 }
