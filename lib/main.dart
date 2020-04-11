7110926283aabcfb413a1a5f97172cd3123301f9
Luke Barratt <luke.barratt@cpwonlinesolutions.com>
2020-04-11 12:09:42 +0100

    Statefull and stateless widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,23 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  int questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer chosen');
+    setState(() {
+      questionIndex += 1;
+    });
+    print(questionIndex);
   }
 
   @override
@@ -20,7 +34,9 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('The question!'),
+              Text(
+                questions[questionIndex],
+              ),
               RaisedButton(
                 child: Text('Answer 1'),
                 onPressed: answerQuestion,
